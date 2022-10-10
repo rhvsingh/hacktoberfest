c6f328763c3837ea9de836bf40db5940fa018a0c
Rishabh Pratap Singh <96458739+iam-Rps@users.noreply.github.com>
2022-10-10 13:22:17 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,52 @@
+import random
+
+def spc(u, c):
+    if u == 'stone':
+        if c == 'paper':
+            return -1
+        elif c == 'scissors':
+            return 1
+        else:
+            return 0
+    elif u == 'paper':
+        if c == 'stone':
+            return 1
+        elif c == 'scissors':
+            return -1
+        else:
+            return 0
+    else:
+        if c == 'paper':
+            return -1
+        elif c == 'stone':
+            return 1
+        else:
+            return 0
+
+user = input()
+#user = tolower(user)
+l = ['stone', 'paper', 'scissors']
+index = random.randint(0, 2)
+comp = l[index]
+print('Computer plays ', comp)
+n = spc(user, comp)
+if n == 1:
+    print('User Won')
+elif n == -1:
+    print('User Lost')
+else:
+    print('It\'s a Tie')
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
